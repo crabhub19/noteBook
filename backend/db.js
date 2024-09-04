99cded1c21b55707063b20f63c529acd7b003931
Bishal <144455368+crabhub19@users.noreply.github.com>
2024-09-04 12:40:26 +0600

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongooseUrl = 'mongodb://localhost:127.0.0.1:27017/mydatabase'
+const mongooseUrl = 'mongodb://127.0.0.1:27017'
 async function connectToDatabase() {
   try {
     await mongoose.connect(mongooseUrl, {
