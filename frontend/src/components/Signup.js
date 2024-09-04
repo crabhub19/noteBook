a563bb86b3859a0949ef545aaaaadade36d18241
Bishal <144455368+crabhub19@users.noreply.github.com>
2024-09-04 22:05:24 +0600

    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -41,7 +41,7 @@ export default function Signup(props) {
   };
   return (
     <>
-      <section className="flex flex-col items-center transition-all duration-1000 bg-signup-Light dark:bg-signup-Dark bg-cover bg-right">
+      <section className="flex flex-col justify-center items-center transition-all duration-1000 bg-signup-Light dark:bg-signup-Dark bg-cover bg-right">
         <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:border-gray-700 backdrop-blur-sm">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
