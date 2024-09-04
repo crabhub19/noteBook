771b25e5225afe1827db65106cff3fc8eab4d48c
Bishal <144455368+crabhub19@users.noreply.github.com>
2024-09-04 10:33:34 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const connectToDatabase = require('./db');
 const express = require('express');
 const cors = require('cors');
-connectToDatabase();
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
